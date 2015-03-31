1ba0147f6e7c1336839508ff12f38cb52ecdae01
mingusone <mingus718@gmail.com>
2015-03-30 20:39:48 -0400

    quicksave

--- node.js
+++ node.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+var app = require('express')();
+var server = require('http').createServer('app');
 var io = require('socket.io')(server);
 
-io.on('connection', function(client) {
+io.on('connection', function(socket){
 	console.log("Client connected");
-	client.emit('messages', {hello:'world'});
+	socket.on('disconnect',function(){
+		console.log("client disconnected");
+	});
 });
 
+/*
+io.on('disconnect', function(client) {
+	console.log("Client disconnected");
+});
+*/
 app.get('/', function(req, res) {
 	//Why wont it send the css and js along with html?
 	res.sendFile(__dirname + '/index.html');
