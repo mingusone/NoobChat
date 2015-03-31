409c335ab6ec60d8a880b67147e1f92516fec053
mingusone <mingus718@gmail.com>
2015-03-31 19:45:30 -0400

    version 1.0

--- node.js
+++ node.js
@@ -2,32 +2,22 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(client){
-	console.log("Client connected");
-	client.on('disconnect',function(socket){
-		console.log("client disconnected");
-	});
-	client.on('NewChat', function(chat){
-		console.log(chat);
-	});
-});
 
-
-/*
-io.on('disconnect', function(client) {
-	console.log("Client disconnected");
-});
-*/
 app.get('/', function(req, res) {
 	//Why wont it send the css and js along with html?
 	res.sendFile(__dirname + '/index.html');
-	//This does not work. 
-	//res.render('index', function(err, html)
-	//	{
-	//		res.send(html);
-	//	});
+});
 
 
+io.on('connection', function(client){
+	//console.log("Client connected");
+	//client.on('disconnect',function(socket){
+	//	console.log("client disconnected");
+	//});
+	client.on('NewChat', function(chat){
+		console.log(chat);
+		io.emit('NewChat', chat);
+	});
 });
 
 server.listen(8888);
